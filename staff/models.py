6a02c7459204a1dc0e1136f711aba23d8786902e
949027 <82878668+949027@users.noreply.github.com>
2022-03-01 14:19:49 +0700

    Tree hierarchy of employees

--- staff/models.py
+++ staff/models.py
@@ -1,7 +1,8 @@
 from django.db import models
+from mptt.models import MPTTModel, TreeForeignKey
 
 
-class Employee(models.Model):
+class Employee(MPTTModel):
     last_name = models.CharField('Фамилия', max_length=100)
     first_name = models.CharField('Имя', max_length=100)
     middle_name = models.CharField('Отчество', max_length=100)
@@ -9,11 +10,14 @@ class Employee(models.Model):
     recruitment = models.DateField('Дата приема на работу')
     salary = models.FloatField('Зарплата')
     level = models.IntegerField('Уровень')
-    chief = models.ForeignKey(
-        'Employee',
+    parent = TreeForeignKey(
+        'self',
         verbose_name='Начальник',
         related_name='subordinates',
         on_delete=models.CASCADE,
         blank=True,
         null=True,
     )
+
+    class MPTTMeta:
+        order_insertion_by = ['last_name']
